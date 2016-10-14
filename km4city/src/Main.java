dd9d8ce16cd88d22180211b29deaf583d21d5c72
filippo.ermini <filippo.ermini@gmail.com>
2016-10-14 18:18:15 +0200

    Aggiunta Serializzazione generica xml e sistema di logging attraverso la libreria log4j

--- km4city/src/Main.java
+++ km4city/src/Main.java
@@ -1,12 +1,19 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 import XMLDomain.Tree;
 import km4city.Application.XMLParsing;
 
 public class Main {
 
+	final static Logger logger = Logger.getLogger(Main.class);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		logger.info("Start computation");
 		XMLParsing<Tree> xml = new XMLParsing<>(Tree.class);
 		Tree tree = xml.DeserializeFromXML("sensor.xml");
+		
 	}
 
 }
--- km4city/src/km4city/Application/XMLParsing.java
+++ km4city/src/km4city/Application/XMLParsing.java
@@ -9,7 +9,10 @@ import java.util.Scanner;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+
+import org.apache.log4j.Logger;
 
 import com.thoughtworks.xstream.XStream;
 
@@ -20,11 +23,31 @@ import com.thoughtworks.xstream.XStream;
 public class XMLParsing<T> {
 	
 	final Class<T> typeParameterClass;
+	final static Logger logger = Logger.getLogger(XMLParsing.class);
 	
 	public XMLParsing(Class<T> typeParameterClass){
 		this.typeParameterClass = typeParameterClass;
 	}
+	public boolean SerializeToXML(String xmlPath, T object){
+		boolean results = false;
+		try {
 
+				File file = new File(xmlPath);
+				JAXBContext jaxbContext = JAXBContext.newInstance(typeParameterClass);
+				Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+	
+				// output pretty printed
+				jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+				jaxbMarshaller.marshal(object, file);
+				logger.info("Serialize "+typeParameterClass+" object into file "+xmlPath);
+				results = true;
+
+		    } catch (JAXBException e) {
+		    	logger.error("Serialize Error - JAXB exception: "+e.getMessage());
+		    }
+		return results;
+
+	}
 	public T DeserializeFromXML(String xmlPath){
 		T object=null;
 		try {
@@ -34,9 +57,10 @@ public class XMLParsing<T> {
 
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 			object = (T) jaxbUnmarshaller.unmarshal(file);
+			logger.info("Deserialize file "+xmlPath+" into "+typeParameterClass+" object");
 
 		  } catch (JAXBException e) {
-			e.printStackTrace();
+			logger.error("Deserialize error - JAXB excepition: "+e.getMessage());
 		  }
 		return object;
 		
